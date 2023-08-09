7d72dd8bbdcaa35699f5e1aa45707e438be9d2ae
dzianis-sudkou <sudkov.denis@gmail.com>
2023-08-09 20:16:09 +0200

    NewTask

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app app>
+    <v-app-bar app color="red darken-3">
+    <v-app-bar-title>
+      <h3 class="text-h4 white--text">
+        To-Do
+      </h3>
+    </v-app-bar-title>
     </v-app-bar>
-
     <v-main>
-      <router-view/>
+      <NewTask/>
     </v-main>
   </v-app>
 </template>
@@ -46,11 +16,15 @@
 
 
 <script>
+import NewTask from './components/NewTask.vue';
 
 export default {
   name: 'App',
   data: () => ({
     //
   }),
+  components:{
+    NewTask
+  }
 };
 </script>
---
+++ src/components/NewTask.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'NewTask',
+    data: () => ({
+        text:''
+    }),
+    methods:{
+        print(){
+            console.log(this.text)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
