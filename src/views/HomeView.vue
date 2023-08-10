a9d224f5e8051d1b18ae477ca769e844619dd1cd
dzianis-sudkou <sudkov.denis@gmail.com>
2023-08-10 12:53:29 +0200

    Starting With Archive Task

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
   <div>
     <h2>Home Page</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis expedita fugit eaque, iusto ad delectus dolorem quos incidunt labore laboriosam, culpa facilis impedit. Eius quae veniam quia, adipisci earum laudantium!</p>
-    
     <router-link to="/tasks">Tasks</router-link>
   </div>
 </template>
